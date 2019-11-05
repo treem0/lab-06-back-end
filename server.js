d7559fea944dc372385f15d301afab19ee45b6a0
Travis Molter <travismolter@gmail.com>
2019-11-05 09:47:58 -0800

    created the basic structure for the lab

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
