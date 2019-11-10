0ed7543962a454748d95a8d19ebeaf8c73da00b2
treem0 <travismolter@gmail.com>
2019-11-09 17:17:12 -0800

    working on yelp and trails

--- server.js
+++ server.js
@@ -43,6 +43,28 @@ const getWeatherResponse = async(lat, long) => {
     });
 };
 
+const getTrailResponse = async(lat, long) => {
+    const trailData = await superagent.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${long}&maxDistance=200&key=${process.env.HIKING_API_KEY}`);
+
+    const actualTrailData = JSON.parse(trailData.text);
+    let trailsArray = actualTrailData.trails;
+    return trailsArray.map(trails => { 
+        return {
+            name: trails.name,
+            location: trails.location,
+            length: trails.length,
+            stars: trails.stars,
+            star_votes: trails.starVotes,
+            summary: trails.summary,
+            trail_url: trails.trail_url,
+            conditions: trails.conditions,
+            condition_date: trails.conditionDate,
+            condition_time: trails.conditionTime
+
+        };
+    });
+};
+
 // const getEventResponse = async(lat, long) => {
 //     const eventData = await superagent.get(`https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.latitude=${lat}&location.longitude=${long}`);
 
@@ -83,6 +105,13 @@ app.get('/weather', async(req, res) => {
     const weatherObject = await getWeatherResponse(latLngs.latitude, latLngs.longitude);
 
     res.json(weatherObject);
+});
+
+app.get('/trails', async(req, res) => {
+    const trailObject = await getTrailResponse(req.query.latitude, req.query.longitude);
+
+    res.json(trailObject);
+
 });
 
 
